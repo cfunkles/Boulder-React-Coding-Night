1b05539dd468d0c636ab48a8e116eac55f7302df
Cfunkles <cfunkles@gmail.com>
2018-03-15 19:40:55 -0600

    create tic-tac toe game

--- src/App.js
+++ src/App.js
@@ -3,13 +3,37 @@ import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cells: ["","","","","","","","",""],
+    };
+  }
+  onCellClick = (i) => {
+    const cells = this.state.cells
+    cells[i] = 'X'
+    this.setState({cells})
+
+    console.log(this.state)
+  }
+
+  aiMove = (cells) => {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <p>Hello, world!</p>
+        <ul className="ticTacBoard">
+          {this.state.cells.map((cell, i)=> {
+            return <li key={i} onClick={()=>this.onCellClick(i)} className="cell">{cell}</li>
+          })}
+      </ul>
       </div>
     );
   }
 }
+
+
 
 export default App;
